4d06edcc9c0dcfe57a276808db3d407de0b356f9
dbBurzum <khalidsr@code.edu.az>
2022-04-14 23:54:47 +0400

    Add project files.

---
+++ ConsoleApp(Football-Team)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_Football_Team_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
