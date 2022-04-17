eafa31f8e30f1d13719721a2eac782ec34cd79ff
dbBurzum <khalidsr@code.edu.az>
2022-04-18 00:13:23 +0400

    newed

--- ConsoleApp(Football-Team)/Program.cs
+++ ConsoleApp(Football-Team)/Program.cs
@@ -13,54 +13,74 @@ namespace ConsoleApp_Football_Team_
 
         static void Main(string[] args)
         {
+            ClubController clubController = new ClubController();
 
-            while (true)
+
+        Entername: Extension.Print(ConsoleColor.Red, "Choose players menu or clubs menu: \n" +
+            "1. Clubs \n" +
+            "2. Players \n" +
+            "0. Quit the app\n" +
+            "");
+            Extension.Print(ConsoleColor.Blue, "Make a choice: ");
+            string num = Console.ReadLine();
+            int input;
+            bool isNum = int.TryParse(num, out input);
+            do
             {
-                ClubController clubController = new ClubController();
-                Extension.Print(ConsoleColor.DarkYellow, "1. Create a club\n" +
-                    "2. Update a club\n" +
-                    "3. Remove a club\n" +
-                    "4. Get all the clubs info \n" +
-                    "5. Search for a club\n" +
-                    "");
-                Extension.Print(ConsoleColor.Magenta, "====================================(End of Menu)==================================== \n" +
-                    "");
 
-                //Entername: Extension.Print(ConsoleColor.Red, "You misspelled something... Try again!");
-                Extension.Print(ConsoleColor.Blue, "Make a choice: ");
-                string num = Console.ReadLine();
-                int input;
-                bool isNum = int.TryParse(num, out input);
-                if (isNum && input>0&&input<7)
+                if (isNum && input > 0 && input < 7)
                 {
 
 
                     switch (input)
                     {
-                        case (int)Extension.Menu.CreateAClub:
-                           clubController.CreateClub();
+                        case 1:
+                            Extension.Print(ConsoleColor.DarkYellow, "1. Create a club\n" +
+                                                                     "2. Update a club\n" +
+                                                                     "3. Remove a club\n" +
+                                                                     "4. Get all the clubs info \n" +
+                                                                     "5. Search for a club\n" +
+                                                                     "");
+                            Extension.Print(ConsoleColor.Magenta, "====================================(End of Menu)==================================== \n" +
+                                "");
+                            int newthing = Convert.ToInt32(Console.ReadLine());
+                            switch (newthing)
+                            {
+                                case (int)Extension.Menu.CreateAClub:
+                                    clubController.CreateClub();
+                                    break;
+                                case (int)Extension.Menu.UpdateAClub:
+                                    clubController.Update();
+                                    break;
+                                case (int)Extension.Menu.RemoveAClub:
+                                    clubController.RemoveAClub();
+                                    break;
+                                case (int)Extension.Menu.GetAllTheClubInfo:
+                                    clubController.GetAllClubs();
+
+                                    break;
+                                case (int)Extension.Menu.SearchForAClub:
+                                    clubController.SearchClub();
+
+                                    break;
+                                case 0:
+                                    break;
+                                default:
+                                    break;
+                            }
                             break;
-                        case (int)Extension.Menu.UpdateAClub:
-                            clubController.Update();
-                            break;
-                        case (int)Extension.Menu.RemoveAClub:
-                            clubController.RemoveAClub();
-                            break;
-                        case (int)Extension.Menu.GetAllTheClubInfo:
-                            clubController.GetAllClubs();
-                            
-                            break;
-                        case (int)Extension.Menu.SearchForAClub:
-                            clubController.SearchClub();
-                            
+                        case 2:
                             break;
                         case 0:
+                            Environment.Exit(0);
                             break;
                         default:
                             break;
                     }
+
                 }
             }
+            while (input != 0);
         }
     }
 }
